db5247c23de68b5ee77229ebe22e169ec7f74b1b
Manuel Diaz <manuel920419@gmail.com>
2022-05-08 23:07:28 -0500

    Commit solve

--- app/Models/ZipCodes.php
+++ app/Models/ZipCodes.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class ZipCodes extends Model
 {
@@ -25,7 +26,5 @@ class ZipCodes extends Model
                             'id_asenta_cpcons',
                             'd_zona',
                             'c_cve_ciudad'];
-
-    protected function zip_code(){}
 
 }
